a46c33ce34ce07fdfb930390de42fd2d444cb5b8
BrandonBtos <brandonbtos@outlook.com>
2024-06-01 00:51:55 -0400

    footer update

--- resources/views/store/includes/menus/footer-menu.blade.php
+++ resources/views/store/includes/menus/footer-menu.blade.php
@@ -2,44 +2,37 @@
     <footer id="footer">
         <div class="container padding-medium ">
         <div class="row">
-            <div class="col-sm-6 col-lg-4 my-3">
+            <div class="col-sm-4 col-lg-2 my-3">
             <div class="footer-menu">
                 <a href="index.html">
-                <img src="{{ asset('build/bootstrap/images/logo.png')}}" alt="logo" class="img-fluid img-thumbnail" style="max-width:100px" >
+                <img src="{{ asset('build/bootstrap/images/icons/logo.jpeg')}}" alt="logo" class="img-fluid" style="max-width:100px" >
                 </a>
                 <div class="social-links mt-4">
-                <ul class="d-flex list-unstyled ">
-                    <li class="me-4">
+                <ul class="d-flex list-unstyled">
+                    <li class="me-2">
                     <a href="#">
-                        <svg class="social-icon" width="30" height="30" aria-hidden="true">
+                        <svg class="social-icon" style="color:#65107f;" width="30" height="30" aria-hidden="true" >
                         <use xlink:href="#facebook"></use>
                         </svg>
                     </a>
                     </li>
-                    <li class="me-4">
+                    <li class="me-2">
                     <a href="#">
-                        <svg class="social-icon" width="30" height="30" aria-hidden="true">
+                        <svg class="social-icon" style="color:#65107f;" width="30" height="30" aria-hidden="true">
                         <use xlink:href="#twitter"></use>
                         </svg>
                     </a>
                     </li>
-                    <li class="me-4">
+                    <li class="me-2">
                     <a href="#">
-                        <svg class="social-icon" width="30" height="30" aria-hidden="true">
+                        <svg class="social-icon" style="color:#65107f;" width="30" height="30" aria-hidden="true">
                         <use xlink:href="#instagram"></use>
                         </svg>
                     </a>
                     </li>
-                    <li class="me-4">
+                    <li class="me-2">
                     <a href="#">
-                        <svg class="social-icon" width="30" height="30" aria-hidden="true">
-                        <use xlink:href="#linkedin"></use>
-                        </svg>
-                    </a>
-                    </li>
-                    <li class="me-4">
-                    <a href="#">
-                        <svg class="social-icon" width="30" height="30" aria-hidden="true">
+                        <svg class="social-icon" style="color:#65107f;" width="30" height="30" aria-hidden="true">
                         <use xlink:href="#youtube"></use>
                         </svg>
                     </a>
@@ -50,77 +43,74 @@
             </div>
             <div class="col-sm-6 col-lg-2 my-3">
             <div class="footer-menu">
-                <h5 class=" fw-bold mb-4">Quick Links</h5>
+                <h5 class=" fw-bold mb-4">Links rápidos</h5>
                 <ul class="menu-list list-unstyled">
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Home</a>
+                    <a href="#" class="footer-link">Inicio</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">About us</a>
+                    <a href="#" class="footer-link">Vuelos</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Courses</a>
+                    <a href="#" class="footer-link">Promociones</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Blogs</a>
+                    <a href="#" class="footer-link">Quienes somos</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Contact</a>
+                    <a href="#" class="footer-link">Contacto</a>
                 </li>
                 </ul>
             </div>
             </div>
             <div class="col-sm-6 col-lg-2 my-3">
             <div class="footer-menu">
-                <h5 class=" fw-bold mb-4">About</h5>
+                <h5 class=" fw-bold mb-4">Acerca de </h5>
                 <ul class="menu-list list-unstyled">
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">How It Works</a>
+                    <a href="#" class="footer-link">Promociones</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Pricing</a>
-                </li>
-                <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Promotion</a>
-                </li>
-                <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Affilation</a>
+                    <a href="#" class="footer-link">Precios</a>
                 </li>
                 </ul>
             </div>
             </div>
-            <div class="col-sm-6 col-lg-2 my-3">
+            <div class="col-sm-6 col-lg-3 my-3">
             <div class="footer-menu">
-                <h5 class=" fw-bold mb-4">Help Center</h5>
+                <h5 class=" fw-bold mb-4">Legales</h5>
                 <ul class="menu-list list-unstyled">
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Payments</a>
+                    <a href="" class="footer-link">Políticas de Cancelación</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">FAQs</a>
+                    <a href="#" class="footer-link">Políticas de Privacidad</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Checkout</a>
+                    <a href="#" class="footer-link">Políticas de Transporte</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">Other</a>
+                    <a href="#" class="footer-link">Terminos y Condiciones</a>
                 </li>
 
                 </ul>
             </div>
             </div>
-            <div class="col-sm-6 col-lg-2 my-3">
+            <div class="col-sm-6 col-lg-2 my-2">
             <div class="footer-menu">
-                <h5 class=" fw-bold mb-4">Contact Us</h5>
+                <h5 class=" fw-bold mb-4">Contactanos</h5>
                 <ul class="menu-list list-unstyled">
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">contact@yourcompany</a>
+                    <a href="#" class="footer-link">ventas@volarenglobo.com.mx</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">+110 4587 2445</a>
+                    <a href="#" class="footer-link">+55 2490 0000</a>
                 </li>
                 <li class="menu-item mb-2">
-                    <a href="#" class="footer-link">New York, USA</a>
+                    <a href="#" class="footer-link">+55 1099 8008</a>
+                </li>
+                <li class="menu-item mb-2">
+                    <a href="#" class="footer-link">+55 51 06 8115</a>
                 </li>
                 </ul>
             </div>
