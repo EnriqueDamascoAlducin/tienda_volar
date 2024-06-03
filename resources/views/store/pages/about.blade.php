4572f996c04430fc8d764f37f432468b4ead3cc8
BrandonBtos <brandonbtos@outlook.com>
2024-06-02 20:05:26 -0400

    page about

---
+++ resources/views/store/pages/about.blade.php
@@ -0,0 +1,162 @@
+<x-guest-layout>
+    <section id="about" class="py-4 mt-xl-5">
+        <div class="container container-about">
+        <div class="row align-items-center mt-xl-5">
+            <div class="col-12 mt-4 mb-5 h2-about ">
+                <h2>¡Toda historia real comienza con un sueño!</h2>
+            </div>
+            <div class="col-md-5 mt-3 about-p">
+                <p class="mx-3 col">
+                    Volar en Globo comienza como un Hobby sin ninguna planificación, 
+                    al darnos cuenta de que además de disfrutar volar teníamos la 
+                    oportunidad de compartir esta experiencia y con el deseo de servir 
+                    y hacer felices a las personas. Poco a poco la demanda fue aumentando
+                    y con ello la necesidad de mejorar nuestras infraestructuras 
+                    para asegurarnos de ofrecer siempre un servicio de calidad. Además, 
+                    generar empleo, transmitir valores como la lealtad, el respeto y la
+                    honestidad que hoy por hoy nos caracteriza.
+                </p>
+             </div>
+            <div class="col-md-5 col-sm-5 col">
+                <img src="{{ asset('build/bootstrap/images/about-2.jpeg')}}" alt="img" class="img-fluid ">
+            </div>
+        </div>
+        </div>
+    </section>
+    <section id="fast-action" class="padding-medium-new">
+        <div class="container">
+            <div class="text-center div-sep-2">
+                <h3 class="text-div text-white">Nuestra mayor motivación es permitir a nuestros clientes vivir esta maravillosa experiencia</h3>
+              </div>
+        </div>
+    </section>
+    <section id="about" class="py-4 mt-xl-5">
+        <div class="container container-about">
+        <div class="row align-items-center mt-xl-5">
+            <div class="col-md-5 col-sm-5 col">
+                <img src="{{ asset('build/bootstrap/images/about-3.jpeg')}}" alt="img" class="img-fluid ">
+            </div>
+            <div class="col-md-5 mt-3 about-p">
+                <p class="mx-3 col">
+                    Para ello contamos con el globo puerto mejor posicionado,
+                     con la mejor vista y ubicación para sobrevolar la Pirámide 
+                     del Sol, pilotos con licencia emitida por la Autoridad Aeronáutica
+                      y aeronaves (globos) 100% seguros. Además, servicios adicionales 
+                      como transporte, restaurante,
+                     tiendas de souvenirs, estacionamiento cerrado con vigilancia.
+                </p>
+             </div>
+        </div>
+        </div>
+    </section>
+    <section class="carousel_se_02">
+        <div class="container-fluid py-4">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-12 px-4 pt-0">
+                <div class="owl-carousel owl-carousel-new carousel_se_02_carousel owl-theme">
+                    <div class="item">
+                    <div class="col-sm-12 p-2 wow fadeInUp delay-3">
+                        <div class="">
+                        <div class="product-image3 hover15">
+                            <img src="{{ asset('build/bootstrap/images/carrousel/1.jpeg')}}" class="img-fluid pic-1" />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="col-sm-12 p-2 wow fadeInUp delay-4">
+                        <div class="">
+                        <div class="product-image3 hover15">
+                            <img src="{{ asset('build/bootstrap/images/carrousel/2.jpeg')}}" class="img-fluid pic-1" />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                    <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                        <div class="">
+                        <div class="product-image3 hover15">
+                            <img src="{{ asset('build/bootstrap/images/carrousel/3.jpeg')}}" class="img-fluid pic-1" />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="item">
+                        <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                            <div class="">
+                            <div class="product-image3 hover15">
+                                <img src="{{ asset('build/bootstrap/images/carrousel/4.jpeg')}}" class="img-fluid pic-1" />
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                <div class="">
+                                <div class="product-image3 hover15">
+                                    <img src="{{ asset('build/bootstrap/images/carrousel/5.jpeg')}}" class="img-fluid pic-1" />
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                    <div class="">
+                                    <div class="product-image3 hover15">
+                                        <img src="{{ asset('build/bootstrap/images/carrousel/6.jpeg')}}" class="img-fluid pic-1" />
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="item">
+                                    <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                        <div class="">
+                                        <div class="product-image3 hover15">
+                                            <img src="{{ asset('build/bootstrap/images/carrousel/7.jpeg')}}" class="img-fluid pic-1" />
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div class="item">
+                                        <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                            <div class="">
+                                            <div class="product-image3 hover15">
+                                                <img src="{{ asset('build/bootstrap/images/carrousel/8.jpeg')}}" class="img-fluid pic-1" />
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                                <div class="">
+                                                <div class="product-image3 hover15">
+                                                    <img src="{{ asset('build/bootstrap/images/carrousel/9.jpeg')}}" class="img-fluid pic-1" />
+                                                </div>
+                                                </div>
+                                            </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-sm-12 p-2 wow fadeInUp delay-2">
+                                                    <div class="">
+                                                    <div class="product-image3 hover15">
+                                                        <img src="{{ asset('build/bootstrap/images/carrousel/10.jpeg')}}" class="img-fluid pic-1" />
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+    <section id="fast-action" class="padding-medium-new">
+        <div class="container">
+            <div class="text-center div-sep-3">
+                <h3 class="text-div text-white">Conoce más sobre nuestros vuelos</h3>
+                <button type="button" class="btn btn-light">VER VUELOS</button>
+              </div>
+        </div>
+    </section>
+</x-guest-layout>
